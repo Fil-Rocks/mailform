ae00b48aada89eb8f2b4b701360fd9f95837f678
Fil-Rocks <fil_freelance@inbox.ru>
2021-02-23 21:18:55 +0300

    mailform

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Отправка mail</title>
 </head>
+
 <body>
     <form action="sandmail.php" method="POST">
-        <section name = "subject">
+        <select name = "subject">
             <option disabled selected>Тема письма</option>
             <option value="1">Вопрос к уроку</option>
             <option value="2">Личный вопрос</option>
             <option value="3">Благодарность</option>
-        </section>
+        </select>
+        <input type="email" name="email" placeholder="Введите ваш email" maxlengtht="50" required>
+        <textarea name="message" placeholder="Введите сообщение" maxlength="150" required></textarea>
+        <img src="capcha1.jpg">
+        <input type="number" name="capcha" placeholder="Введите ответ" maxlength="3" required>
+        <input type="submit" value="Отправить письмо">
     </form>
 </body>
 </html>
---
+++ sandmail.php
@@ -0,0 +1,32 @@
+<?php
+if($_POST['capcha'] != 56) {
+    header('location: index.php');
+    exit;
+}
+
+if($_POST['subject'] == 1) {
+    $subject = 'Вопрос по уроку';
+} elseif($_POST['subject'] == 2) {
+    $subject = 'Личный вопрос';
+} elseif($_POST['subject'] == 3) {
+    $subject = 'Благодарность';
+} else {
+    $subject = 'Вопрос по уроку';
+}
+
+$to = "fil_rocks@mail.ru";
+$from = trim($_POST['email']);
+
+$message = htmlspecialchars($_POST['message']);
+$message = urldecode($message);
+$message = trim($message);
+
+$headers = "From: $from" . "\r\n" . 
+"Reply-To: $from" . "\r\n" . 
+"X-Mailer: PHP/" . phpversion();
+
+if(mail($to, $subject, $message, $headers)) {
+    echo "Сообщение отправлено";
+} else {
+    echo "Сообщение не отправлено";
+}
