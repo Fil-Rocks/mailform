0de067a6f1613d0fcbd934fad14f23aef5a3a39f
Fil-Rocks <fil_freelance@inbox.ru>
2021-02-23 05:02:26 +0300

    check

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sandmail.php" method="POST">
+        <section name = "subject">
+            <option disabled selected>Тема письма</option>
+            <option value="1">Вопрос к уроку</option>
+            <option value="2">Личный вопрос</option>
+            <option value="3">Благодарность</option>
+        </section>
+    </form>
+</body>
+</html>
